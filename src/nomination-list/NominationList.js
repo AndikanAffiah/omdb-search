f8ca6afd8edb6b1ecfa00b9f94942b17cc07dc42
AndikanAffiah <andikanaffiah@gmail.com>
2021-01-17 16:25:13 +0100

    fixed nomination bug and minor UI improvements

--- src/nomination-list/NominationList.js
+++ src/nomination-list/NominationList.js
@@ -6,18 +6,15 @@ export default function NominationList (props) {
         <div>
             <h4 className="mb-4 d-flex justify-between">
                 <div>Nominations List</div>
-                {/* <div>
-                    <button onClick={e=>(props.saveToDB(e))} className="color-white cursor-pointer p-2 border-1 bg-l-blue">Save List</button>
-                    </div> */}
             </h4>
             <div className="list h-400-px">
-            {props.nominations.map((movie, index) => (
+            { props.nominations.length > 0 ? props.nominations.map((movie, index) => (
                 <div className="d-flex suggestion-list border-1 bs-1-1-5 mb-4 br-soft" key={index}>
                 <div className="img py-1">
                     <img
                     id="imt"
                     width="50"
-                    src={movie.Poster}
+                    src={movie.Poster === "N/A"? "no-img.jpg": movie.Poster}
                     alt="movie_icon"
                     />
                 </div>
@@ -28,14 +25,13 @@ export default function NominationList (props) {
                     <div className="px-2 w-90 d-flex justify-center">
                         <button 
                         onClick={e => {
-                            //remove to db
                             props.db.movies.delete(movie.imdbID);
                             props.setReRenderState(true);
-                        }} className="p-1 w-50 bg-brick color-white br-soft border-1 cursor-pointer">Remove</button>
+                        }} className="p-1 w-50 bg-brick color-white br-soft border-1 cursor-pointer fs-md-12">Remove</button>
                     </div>
                 </div>
             </div>
-            ))}
+            )) : <div>No nominations yet, try searching..</div>}
             </div>
         </div>
     );
